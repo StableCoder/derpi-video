9f92de1e6215856272bc253bfaa68a576b47be60
George Cave <gcave@stablecoder.ca>
2019-08-17 13:19:39 -0400

    Expanded the remove_formats script
    It now works properly on arbitrary JSON files, and will first write to a
    temporary file first, before renaming back to the original.

--- remove_formats.py
+++ remove_formats.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
-import json
+# The file to remove the 'formats' section from is given as the program's single argument
 
-with open('a.json') as f:
+import json
+import sys
+import os
+
+# Write out the json w/o the 'formats' section into the same file but appended with the '.temp'
+with open(sys.argv[1]) as f:
     d = json.load(f)
     d['formats'] = []
-    with open('b.json', 'w') as outFile:
-        json.dump(d, outFile)
+    with open(sys.argv[1] + '.temp', 'w') as outFile:
+        json.dump(d, outFile, indent=4)
+
+# Rename that temp file back to the original file, replacing it.
+os.rename(sys.argv[1] + '.temp', sys.argv[1])
