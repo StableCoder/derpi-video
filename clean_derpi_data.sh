68a6c61e08afa71845cd2b655cfcee5e3c9b0285
George Cave <gcave@stablecoder.ca>
2019-08-18 05:46:35 -0400

    Added file to clean derpi data

---
+++ clean_derpi_data.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+# This quick script just cleans all the .derpi.json files found within the
+# target directory, recursively
+
+TARGET_DIR=$(pwd)
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -t|--target)
+    TARGET_DIR="$2"
+    shift
+    shift
+    ;;
+    *)    # unknown option
+    ;;
+esac
+done
+
+shopt -s globstar
+rm -rf $TARGET_DIR/**/*.derpi.json
