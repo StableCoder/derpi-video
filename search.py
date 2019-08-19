7267baccdf694555c2706dd86cf8d33b37a5ce8e
George Cave <gcave@stablecoder.ca>
2019-08-19 17:55:13 -0400

    Some more work on search mekanism

--- search.py
+++ search.py
@@ -2,7 +2,20 @@ from flask import Response
 from pymongo import MongoClient
 
 
-def entry_return_basic(video_entry):
+def entry_return_summary(video_entry):
+    """
+    Returns the basic video information, good for a summary
+
+    Parameters
+    ----------
+    video_entry
+        The full dict entry of the video that was retrieved from the database
+
+    Returns
+    -------
+    dict
+        Dict of just the summary data
+    """
     youtube_data = video_entry['youtube_data']
     derpi_data = video_entry['derpi_data']
     entry = {}
@@ -14,33 +27,77 @@ def entry_return_basic(video_entry):
     entry['view_count'] = youtube_data['view_count']
     entry['average_rating'] = youtube_data['average_rating']
     entry['thumbnail'] = derpi_data['thumbnail']
+    entry['status'] = derpi_data['status']
     return entry
 
 
-def id_search(searchList):
+def entry_return_detail(video_entry):
+    """
+    Returns the full video data for public consumption
+
+    This is all the information from get_entry_summary() plus more in-depth fields.
+
+    Parameters
+    ----------
+    video_entry
+        The full dict entry of the video that was retrieved from the database
+
+    Returns
+    -------
+    dict
+        Dict of detailed data
+    """
+    entry = entry_return_summary(video_entry)
+    youtube_data = video_entry['youtube_data']
+    # Add the full data
+    entry['description'] = youtube_data['description']
+    return entry
+
+
+def get_db():
     client = MongoClient('localhost', 27017)
-    db = client.archive
-    foundCount = 0
+    return client.archive
+
+
+def id_search(single_search_term):
+    """
+    Performs a search for a video ID using the single given string
+
+    Parameters
+    ----------
+    single_search_term : string
+        Search term to try to match with a video ID
+
+    Returns
+    -------
+    []dict
+        Array of one dict with data, if found. Empty otherwise.
+    """
+    db = get_db()
+    found = db.video.find({"youtube_data.id": it})
     retList = []
-    for it in searchList:
-        found = db.video.find({"youtube_data.id": it})
-        foundCount += found.count()
-        for fIt in found:
-            retList.append(entry_return_basic(fIt))
-    return foundCount, retList
+    for it in found:
+        retList.append(entry_return_summary(it))
+    return retList
 
 
 def basic_or_search(searchList):
-    client = MongoClient('localhost', 27017)
-    db = client.archive
-    findCount = 0
+    db = get_db()
     retList = []
-    finds = db.video.find({"$or": [
-        {"youtube_data.title": {"$regex": "Daniel Ingram"}}]})
-    findCount += finds.count()
+    finds = db.video.find(
+        {"$or": [{"youtube_data.title": {"$regex": "Daniel Ingram"}}]})
     for it in finds:
-        retList.append(entry_return_basic(it))
-    return findCount, retList
+        retList.append(entry_return_summary(it))
+    return finds.count(), retList
+
+
+def youtube_tag_search(searchList):
+    db = get_db()
+    finds = db.video.find({'youtube_data.tags': 'gameboy'})
+    retList = []
+    for it in finds:
+        retList.append(entry_return_summary(it))
+    return finds.count(), retList
 
 
 def search_endpoint(url_path):
@@ -53,9 +110,8 @@ def search_endpoint(url_path):
     returnDict['results'] = []
     if queryPos != -1:
         searchList = options[queryPos+2:].split('+')
-        found, foundList = basic_or_search(searchList)
+        found, foundList = youtube_tag_search(searchList)
         if found > 0:
             returnDict['total'] += found
             returnDict['results'].append(foundList)
-
     return returnDict
